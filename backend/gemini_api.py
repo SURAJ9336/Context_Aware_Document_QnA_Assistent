0703ba2f4cef3755e2439508783d33dcf83af295
Suraj Kumar <surajpathak7112002@gmail.com>
2025-09-03 01:20:23 +0530

    updated gemini model

--- backend/gemini_api.py
+++ backend/gemini_api.py
@@ -3,7 +3,7 @@ import os
 
 # Configure Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def ask_gemini(query, chunks):
     context = "\n".join(chunks)
